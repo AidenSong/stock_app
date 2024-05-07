def50e9301598a6e72a7d62247339462dc1c23c7
AIdenVolk <kiset200@naver.com>
2024-05-07 23:52:54 +0900

    2024-05-02

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:stock_app/data/repository/stock_repository_impl.dart';
 import 'package:stock_app/data/source/local/company_listing_entity.dart';
+import 'package:stock_app/data/source/local/stock_dao.dart';
+import 'package:stock_app/data/source/remote/stock_api.dart';
+import 'package:stock_app/presentation/company_listings/company_listings_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:stock_app/presentation/company_listings/company_listings_view_model.dart';
 
 import 'package:stock_app/util/theme.dart';
 
@@ -8,7 +14,21 @@ void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(CompanyListingEntityAdapter());
 
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => CompanyListingsViewModel(
+            StockRepositoryImpl(
+              StockAPI(),
+              StockDao(),
+            ),
+          ),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -28,7 +48,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.dark(),
       ),
       themeMode: ThemeMode.system,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const CompanyListingsScreen(),
     );
   }
 }
